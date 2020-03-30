c838d02f2c03868924490a8de43ea890aa7349ac
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-03-30 15:02:59 -0300

    Categoria Criada e Testando Rest ok

---
+++ src/main/java/com/wyden/bibi/model/Categoria.java
@@ -0,0 +1,67 @@
+package com.wyden.bibi.model;
+
+import java.io.Serializable;
+
+public class Categoria  implements Serializable  {
+	private static final long serialVersionUID = 1L;
+	
+	
+	Integer id_categoria;
+	String nome_categoria;
+	
+	
+	public Categoria() {
+		
+		
+	}
+	
+	public Categoria(Integer id_categoria, String nome_categoria) {
+		super();
+		this.id_categoria = id_categoria;
+		this.nome_categoria = nome_categoria;
+	}
+
+	public Integer getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(Integer id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public String getNome_categoria() {
+		return nome_categoria;
+	}
+
+	public void setNome_categoria(String nome_categoria) {
+		this.nome_categoria = nome_categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_categoria == null) ? 0 : id_categoria.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id_categoria == null) {
+			if (other.id_categoria != null)
+				return false;
+		} else if (!id_categoria.equals(other.id_categoria))
+			return false;
+		return true;
+	}
+	
+	
+
+}
