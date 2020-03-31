13a181b681113245d72938ea7d6d75f4bc6a2a6f
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-03-31 12:12:31 -0300

    Criando camada repositorio e servico para Categoria

--- src/main/java/com/wyden/bibi/resources/CategoriaResource.java
+++ src/main/java/com/wyden/bibi/resources/CategoriaResource.java
@@ -3,26 +3,31 @@ package com.wyden.bibi.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wyden.bibi.model.Categoria;
+import com.wyden.bibi.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-
-		Categoria cat1 = new Categoria(1, "Calculo 1");
-		Categoria cat2 = new Categoria(2, "Engenharia de Software");
-
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	//metodo que encapsula todas as erequisicoes HTTP. ? para dizer que pode ser qualquer um.
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+         
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+		
 	}
 
 }
